a23a7cbb9f301ed89562cc06d14183af48de5199
Anindya Basu <akbasu@gmail.com>
2016-09-10 01:17:04 -0500

    Cleaned up unused code.

--- index.js
+++ index.js
@@ -21,13 +21,3 @@ var server = app.listen(3000, function(){
     console.log('Server running at http://127.0.0.1:3000/');
 });
 
-/*http.createServer(
-    function(req, res){
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:3000/');*/
-
